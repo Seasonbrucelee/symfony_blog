ebed6af19da6bd9ccd683a9aa75d7f001e5e35a2
Seasonbrucelee <sezen39@hotmail.com>
2022-04-12 10:45:49 +0200

    codage des titres en créant un tableau dans le controller, et codage en twig

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -18,9 +18,13 @@ class PostController extends AbstractController
 
     #[Route('/post/{id}', name: 'post_view')] //was app_post
     public function post($id): Response
-    {
+    {   
         return $this->render('post/view.html.twig', [
-            'controller_name' => 'PostController',
+            'post' => [
+            'title' => 'Le titre de l\'article',
+            'content' => 'D\'où vient-il?
+            L\'extrait standard de Lorem Ipsum utilisé depuis le XVIè siècle est reproduit ci-dessous pour les curieux. Les sections 1.10.32 et 1.10.33 du "De Finibus Bonorum et Malorum" de Cicéron sont aussi reproduites dans leur version originale, accompagnée de la traduction anglaise de H. Rackham (1914).'
+            ],
         ]);
     }
 }
